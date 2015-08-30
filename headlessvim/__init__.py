9d7b05af683d14cfdf5d2730e7b4d9f71a1ce004
Ryosuke Ito <rito.0305@gmail.com>
2015-08-31 02:44:07 +0900

    change version to 0.0.2

--- headlessvim/__init__.py
+++ headlessvim/__init__.py
@@ -4,4 +4,4 @@
 from .headlessvim import * # flake8: noqa
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='headlessvim',
-    version='0.0.1',
+    version='0.0.2',
     description='programmable Vim, no need of +clientserver!',
     long_description=open('README.rst').read(),
     keywords='vim test',
